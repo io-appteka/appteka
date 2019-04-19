685bd25f23a403f986a9bb3cd3ab68a54392b0bc
gravvy23 <majkutw@gmail.com>
2019-04-19 17:40:55 +0200

    card component added

--- client/src/containers/DrugList/DrugList.jsx
+++ client/src/containers/DrugList/DrugList.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Card } from '../../components/Card/Card';
+import styles from './DrugList.css';
 
 export class DrugList extends React.Component {
     render() {
         return (
-            <div>DrugList</div>
+            <div className={styles.DrugList}>
+                <Card>DrugList</Card>
+            </div>
         );
     }
 }
