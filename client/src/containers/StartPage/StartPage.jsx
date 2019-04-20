fd0a6512cb722d3fb23a713cff7822f933809c2f
gravvy23 <majkutw@gmail.com>
2019-04-20 19:49:20 +0200

    auto-complete to intro input added

--- client/src/containers/StartPage/StartPage.jsx
+++ client/src/containers/StartPage/StartPage.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import styles from './StartPage.css';
-import { Input } from 'antd';
+import { FirstSearch } from '../../components/UI/Search/FirstSearch/FirstSearch';
 
 export class StartPage extends React.Component {
+    state = {
+        drugList: [],
+    }
+
+    componentDidMount() {
+        //fetching data from server
+        //list of all drug names 
+        this.setState({
+            drugList: ["Acenol", "Acenol forte", "Aglan 15", "Aleve", "Alka-Prim", "Alka-Seltzer", "Amol", "Anapran Neo", "Antidol 15", "Apap Extra", "Apap Noc", "Apap, tabletki powlekane", "Aprofen", "Aspicam", "Aspirin", "Activ", "Aulin saszetki", "Aulin tabletki", "Bi-Profenid", "Cefalgin",  "Codipar", "Codipar 250, -500", "Dexak", "Dexak SL", "Dicloberl retard", "Doreta", "Efferalgan czopki", "Efferalgan tabletki", "Efferalgan", "Vitamin C", "Epiduo", "Erka", "Etopiryna", "Etopiryna Kontrol", "Gemipar 50, -125, -500", "Ibalgin", "Ibufen Forte", "Ibufen Total", "Ibufen zawiesina", "Ibum", "Ibum Forte", "Ibupar", "Ibuprofen Aflofarm", "Ibuprofen Pabi", "Ibuprom", "IBUPROM MAX", "Ketonal forte", "Kidofen duo", "Kodeina", "Kofepar", "Kopiryna", "Malupar 125, -250, -500", "Metafen", "MIG", "Migea", "Movalis", "Nalgesin", "Natrax 100, -200", "Nimesil", "Padolten", "Panadol", "Panadol dla dzieci"]
+        })
+    }
+
     clickHandler = (value) => {
         const { history } = this.props;
         // search propably with query parameters 
@@ -24,11 +36,10 @@ export class StartPage extends React.Component {
             <div className={styles.MainPage}>
                 <div className={styles.Header}>No more overpriced sheeeit, find cheap meds nearby</div>
                 <div className={styles.SmallHeader}>Sheit, man, that honkey mus' be messin' my old lady got to be runnin' col' upsihd down his head!</div>
-                <Input.Search
-                    placeholder="enter drug name..."
-                    enterButton="search"
-                    onSearch={value => this.clickHandler(value)}
-                />
+            <FirstSearch
+                data={this.state.drugList}
+                onClick={this.clickHandler}
+            />
             </div>
         );
     }
