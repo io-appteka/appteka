667628bc428637bfce7c778b0b581e8b7e348cb8
gravvy23 <majkutw@gmail.com>
2019-04-28 12:35:14 +0200

    SearchField imporvements

--- client/src/components/Lists/PricesList/PriceItem/PriceItem.jsx
+++ client/src/components/Lists/PricesList/PriceItem/PriceItem.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './PriceItem.css';
 import { Icon } from 'antd';
+import { NavLink } from 'react-router-dom';
 
 export class PriceItem extends React.Component {
     state = {
@@ -12,16 +13,20 @@ export class PriceItem extends React.Component {
         this.setState({isOpened: !opened});
     };
 
+    //zglos inna cene - nawigacja czy popup z fromularzem
     render(){
         const { pharmacyName, price } = this.props;
         const { isOpened } = this.state;
         return (
             <li className={styles.PriceItem} onClick={this.clickHandler}>
-                <div style={{alignItems: 'center', display: 'flex', justifyContent: 'space-between'}}>
+                <div className={styles.FlexBox}>
                     <div className={styles.PharmacyName}>{pharmacyName}</div>
                     <div className={styles.Price}>{price}<small> zł</small></div>
                 </div>
-                <div className={styles.Locations}>locations{isOpened ? <Icon type="up"/> : <Icon type="down"/>}</div>
+                <div className={styles.FlexBox}>
+                    <div className={styles.Locations}>lokalizacje{isOpened ? <Icon type="up"/> : <Icon type="down"/>}</div>
+                    <NavLink className={styles.ReportLink} to='/report'>zgłoś inną cenę</NavLink>
+                </div>
                 {isOpened && <div>Opened</div>}
             </li>
         );
