014e7b71c38bc7fad9e5c9568d4ae64ce96259a8
papryk <patryknng@interia.pl>
2019-05-11 11:31:16 +0200

    Create server project

---
+++ server/server/Models/Drug/Drug.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace server.Models
+{
+    public class Drug
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        // Ingredients
+        public int Rating { get; set; }
+        // Opinions
+    }
+}
