11bcca6d9ab3813dafa9ee312121d528ed0a2ef0
gravvy23 <majkutw@gmail.com>
2019-05-13 18:44:29 +0200

    Rating comp. added

---
+++ client/src/components/UI/Rating/Rating.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Rate } from 'antd';
+import styles from './Rating.css';
+
+export const Rating = ({value}) => (
+    <div className={styles.Rating}>
+    <Rate prefixCls={'index__ant-rate'} disabled allowHalf defaultValue={value}/>
+    </div>);
