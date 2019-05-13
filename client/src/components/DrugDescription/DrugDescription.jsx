0cb6a2dc563bb9717fad7ec568bcde554fa31216
gravvy23 <majkutw@gmail.com>
2019-05-13 19:03:51 +0200

    Tag comp. added

--- client/src/components/DrugDescription/DrugDescription.jsx
+++ client/src/components/DrugDescription/DrugDescription.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import styles from './DrugDescription.css';
+import { Tag } from '../UI/Tag/Tag';
 
 export const DrugDescription = ({drugInfo}) => (
     <div className={styles.DrugDescription}>
         <h1>{drugInfo.name}</h1>
         <p>{drugInfo.desc}</p>
+        <Tag>Na katar</Tag><Tag>Na kaszel</Tag>
     </div>
 );
