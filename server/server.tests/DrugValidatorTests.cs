7232d94c2a74d206dcb229743f655198a30c76ad
papryk <patryknng@interia.pl>
2019-05-15 01:54:08 +0200

    Add drug validator class and tests

---
+++ server/server.tests/DrugValidatorTests.cs
@@ -0,0 +1,43 @@
+﻿using server.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using server.Data;
+
+namespace server.tests
+{
+    public class DrugValidatorTests
+    {
+        [Theory()]
+        [InlineData("nowy lek")]
+        [InlineData("Jakis tam")]
+        [InlineData("Inny")]
+        public void IsDrugValid_ShouldSuccess(string name)
+        {
+            Drug drug = new Drug()
+            {
+                Name = name
+            };
+
+            bool isValid = DrugValidator.IsDrugValid(drug);
+            Assert.True(isValid);
+        }
+
+        [Theory()]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("  ")]
+
+        public void IsDrugValid_ShouldFail(string name)
+        {
+            Drug drug = new Drug()
+            {
+                Name = name
+            };
+
+            bool isValid = DrugValidator.IsDrugValid(drug);
+            Assert.False(isValid);
+        }
+    }
+}
