71db4230a10dd3279907f15ef2a0a0049ef0a2be
gravvy23 <majkutw@gmail.com>
2019-05-26 22:42:57 +0200

    AddOpinion comp. added

--- client/src/components/UI/Rating/Rating.jsx
+++ client/src/components/UI/Rating/Rating.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { Rate } from 'antd';
 import styles from './Rating.css';
 
-export const Rating = ({value, color}) => (
+export const Rating = ({value, color, disabled=true, ...other}) => (
     <div className={styles.Rating}>
-        <Rate prefixCls={'index__ant-rate'} disabled allowHalf defaultValue={0} value={value} style={{color: color}}/>
+        <Rate prefixCls={'index__ant-rate'} disabled={disabled} allowHalf value={value} style={{color: color}}
+        {...other}/>
     </div>
 );
