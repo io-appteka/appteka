f738834ed844c7ad81c4d34d498546b34df2782e
gravvy23 <majkutw@gmail.com>
2019-05-27 19:09:28 +0200

    AddOpinion final commit

--- client/src/components/Opinions/AddOpinion/AddOpinion.jsx
+++ client/src/components/Opinions/AddOpinion/AddOpinion.jsx
@@ -6,20 +6,20 @@ import { Rate, Input } from 'antd';
 
 const { TextArea } = Input;
 
-export const AddOpinion = ({modal, onCancelHandler, onOkHandler}) => (
+export const AddOpinion = ({modal, onCancelHandler, onOkHandler}) => {
+  const [rate, setRate] = React.useState(0);
+  const [opinion, setOpinion] = React.useState('');
+  return (
     <Modal className={styles.AddOpinion}
     title="Dodaj opinię o produkcie"
     visible={modal.show}
     onCancel={onCancelHandler}
     footer={[
-        <Button key="back" prefixCls="index__ant-btn" onClick={onCancelHandler}>
-          Anuluj
-        </Button>,
-        <Button key="submit" type="primary" prefixCls="index__ant-btn" loading={modal.loading} onClick={onOkHandler}>
-          Opublikuj
-        </Button>,
+        <Button key="back" prefixCls="index__ant-btn" onClick={onCancelHandler}>Anuluj</Button>,
+        <Button key="submit" type="primary" prefixCls="index__ant-btn" loading={modal.loading} onClick={() => onOkHandler({rate, opinion})}>Opublikuj</Button>,
     ]}>
-        <Rate className={styles.OpinionRate} prefixCls={'index__ant-rate'} allowHalf/>
-        <TextArea rows={6} placeholder={'Podziel się swoimi doświadczeniami'}/>
+        <Rate onChange={setRate} className={styles.OpinionRate} prefixCls={'index__ant-rate'} allowHalf/>
+        <TextArea onChange={(event) => setOpinion(event.target.value)} rows={6} placeholder={'Podziel się swoimi doświadczeniami'}/>
     </Modal>
-)
+  );
+};
