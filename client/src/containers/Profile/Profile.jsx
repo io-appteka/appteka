dc29487034755f557622e6b328f18a50cf3c9d16
Weronika Schabowicz <48695124+veronimineral@users.noreply.github.com>
2019-06-02 15:27:40 +0200

    Update Profile.jsx

--- client/src/containers/Profile/Profile.jsx
+++ client/src/containers/Profile/Profile.jsx
@@ -3,11 +3,34 @@ import { Card } from '../../components/Card/Card';
 import styles from './Profile.css';
 
 export class Profile extends React.Component {
+
+    
     render() {
         return (
-            <div className={styles.Profile}>
-                <div className={styles.ProfileBox}>profile</div>
-            </div>
+              <div className={styles.Profile}>
+                <div className={styles.ProfileBox}>
+                  <div className={styles.grid}>
+  <div className={styles.box2}><div className={styles.Username}>Wiesława <span>/ Profil użytkownika</span></div></div>
+  <div className={styles.box3}><ul>
+                           <li><button>Dane kontaktowe</button></li>
+                           <li><button>Opinie</button></li>
+                           <li><button>Zmień hasło</button></li>
+                           <li><button>Usuń konto</button></li>
+                       </ul></div>
+                     
+  <div className={styles.box4}><ul>
+                           <li>Nazwa użytkownika</li>
+                           <li>Adres e-mail</li>
+                           <li>Lokalizacja</li>
+                       </ul></div>
+                       <div className={styles.box5}><ul>
+                           <li>Wiesława</li>
+                           <li>wiesia53@amorki.pl</li>
+                           <li>Kraków, Polska</li>
+                       </ul></div>
+</div>
+                </div></div>
+            
         );
     }
 }
