c4f2da55f9d6160341185b73ed0d928d761e7029
Weronika Schabowicz <48695124+veronimineral@users.noreply.github.com>
2019-06-11 15:19:50 +0200

    Connect Opinion component

--- client/src/containers/Profile/ProfileOpinions.jsx
+++ client/src/containers/Profile/ProfileOpinions.jsx
@@ -1,29 +1,34 @@
 import React from 'react';
 import styles from './ProfileOpinions.css';
+import { Opinion } from '../../components/Opinions/Opinion/Opinion';
 
 export class ProfileOpinions extends React.Component {
+    
+    
     render() {
         return (
 <div className={styles.ProfileOpinions}>
-      <div className={styles.OpinionBox}>
-          <h6>12.03.2019</h6>
-          <h3>Prednizon</h3>
-          <p>Zamówiłam 3 burgery- z wołowiną kurczakiem i vege ale wszystkie dostarczone były z wołowiną. Jako że nie jem mięsa trudno ocenić smak bo na obiad jadłam samą bułkę ale podobno te z mięsem były smaczne</p>
-      </div>
-      <div className={styles.OpinionBox}>
-          <h6>08.11.2018</h6>
-          <h3>Prednizon 2</h3>
-      </div>
-      <div className={styles.OpinionBox}>
-          <h6>16.10.2018</h6>
-          <h3>Strepsils intensive</h3>
-      </div>
-      <div className={styles.OpinionBox}>
-          <h6>02.01.2018</h6>
+     <div className={styles.OpinionBox}>
           <h3>Amoxicillin</h3>
-          <p>Zamówiłam 3 burgery- z wołowiną kurczakiem i vege ale wszystkie dostarczone były z wołowiną. Jako że nie jem mięsa trudno ocenić smak bo na obiad jadłam samą bułkę ale podobno te z mięsem były smaczne</p>
+          <Opinion date='02.01.2018' text='Jakas opinia, nie wazne jaka, proszę scrollować dalej' rating="2.5"/>
+</div>
+      <div className={styles.OpinionBox}>
+        <h3>Prednizon</h3>
+          <Opinion date='12.05.2019' text='Zamówiłam 3 burgery- z wołowiną kurczakiem i vege ale wszystkie dostarczone były z wołowiną. Jako że nie jem mięsa trudno ocenić smak bo na obiad jadłam samą bułkę ale podobno te z mięsem były smaczne' rating="3"/>
+    </div>
+      <div className={styles.OpinionBox}>
+          <h3>Strepsils intensive</h3>
+          <Opinion date='08.11.2018' text='Zamówiłam 3 burgery- z wołowiną kurczakiem i vege ale wszystkie dostarczone były z wołowiną. Jako że nie jem mięsa trudno ocenić smak bo na obiad jadłam samą bułkę ale podobno te z mięsem były smaczne' rating="2.5"/>
+
+            </div>
+      <div className={styles.OpinionBox}>
+          <h3>No-spa</h3>
+          <Opinion date='16.10.2018' text='Zamówiłam 3 burgery- z wołowiną kurczakiem i vege ale wszystkie dostarczone były z wołowiną. Jako że nie jem mięsa trudno ocenić smak bo na obiad jadłam samą bułkę ale podobno te z mięsem były smaczne' rating="4"/>
+
       </div>
-            </div>     
+            </div>   
+  
+                
         );
     }
 }
