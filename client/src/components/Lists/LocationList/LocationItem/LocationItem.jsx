3607616e04570c438d803d043833c49acdbfa489
emanuel gawenda <emanuel.gawenda@teleport.pl>
2019-06-20 12:14:04 +0000

    Map's modal is working properly

--- client/src/components/Lists/LocationList/LocationItem/LocationItem.jsx
+++ client/src/components/Lists/LocationList/LocationItem/LocationItem.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import styles from './LocationItem.css';
-import { Icon } from 'antd';
-import { withRouter } from "react-router"
+import {Icon} from 'antd';
+import {withRouter} from "react-router";
+import {Map} from "../../../Map/Map";
 
 const locationItem = ({info, history}) => {
+    const [show, setShow] = React.useState(false);
+
     const onClickHandler = () => {
-        const queryString = `id=${encodeURIComponent(info.pharmacyStoreId)}`;
-        history.push({
-            pathname: '/store',
-            search: '?' + queryString,
-        });
+        setShow(!show);
+        console.log(show);
     }
 
-    return(
-    <li className={styles.LocationItem}>
-        <div className={styles.Info}><span>{info.address}</span></div>
-        <div className={styles.Info}><span>{info.distance} km</span></div>
-        <div className={styles.Info}><span>{info.openingHours}</span></div>
-        <div className={styles.Info}><span>tel. {info.phone}</span></div>
-        <button className={styles.Button} onClick={onClickHandler}><Icon type="environment" theme="filled" />Mapa</button>
-        <button className={styles.Button} onClick={onClickHandler}><Icon type="info-circle" theme="filled" />Szczegóły</button>
-    </li>
-)};
+    return (
+        <li className={styles.LocationItem}>
+            <div className={styles.Info}><span>{info.address}</span></div>
+            <div className={styles.Info}><span>{info.distance} km</span></div>
+            <div className={styles.Info}><span>{info.openingHours}</span></div>
+            <div className={styles.Info}><span>tel. {info.phone}</span></div>
+            <button className={styles.Button} onClick={onClickHandler}><Icon type="environment" theme="filled"/>Mapa</button>
+            <Map show={show} x={info.x} y={info.y} onExit={onClickHandler}/>
+        </li>
+    )
+};
 
 export const LocationItem = withRouter(locationItem);
